5a0c204b3ea007b33b3f95463564ac29923d4b3e
Dredipatadija <andrearmra@gmail.com>
2024-06-20 15:04:06 +0200

    upload script2

--- script.sh
+++ script.sh
@@ -2,28 +2,21 @@
 
 architecture=$(uname -a)
 
-cpuphy=$(grep "physical id" /proc/cpuinfo | wc -l)
+cpu_phy=$(grep "physical id" /proc/cpuinfo | wc -l)
 
-cpuvir=$(grep "processor" /proc/cpuinfo | wc -l)
+cpu_vir=$(grep "processor" /proc/cpuinfo | wc -l)
 
 ram_available=$(free --mega | awk '$1 == "Mem:" {print $7}')
 
-ram__total=$(free --mega | awk '$1 == "Mem:" {print $2}')
-ram_used=$(free --mega | awk '$1 == "Mem:" {print $3}')
-ram_percent=$(free --mega | awk '$1 == "Mem:" {printf("%.2f%%", ($3/$2)*100)}')
+ram_percent=$(free --mega | awk '$1 == "Mem:" {printf("%.2f", ($3/$2)*100)}')
 
-# DISK
-disk_total=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_t += $2} END {printf ("%.1fGb\n"), disk_t/1024}')
-disk_use=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {print disk_u}')
-disk_percent=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t+= $2} END {printf("%d"), disk_u/disk_t*100}')
+disk_available=$(df -m | grep "^/dev/" | grep -v "/boot" | awk '{disk_a += $4} END {printf("%.2fGb", disk_a/1024)}')
 
-# CPU LOAD
-cpul=$(vmstat 1 2 | tail -1 | awk '{printf $15}')
-cpu_op=$(expr 100 - $cpul)
-cpu_fin=$(printf "%.1f" $cpu_op)
+disk_percent=$(df -m | grep "^/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t+= $2} END {printf("%.2f", disk_u/disk_t*100)}')
 
-# LAST BOOT
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+cpu=$(vmstat 1 2 | tail -1 | awk '{printf("%.2f", (100 - $15))}')
+
+last_boot=$(who -b | awk '$1 == "arranque" {print $4 " " $5}')
 
 # LVM USE
 lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)
@@ -42,17 +35,14 @@ mac=$(ip link | grep "link/ether" | awk '{print $2}')
 cmnd=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
 wall "  Architecture: $arch
-        CPU physical: $cpuf
-        vCPU: $cpuv
-        Memory Available: $ram_available
-        Memory Usage: $ram_percent
-        Disk Usage: $disk_use/${disk_total} ($disk_percent%)
-        CPU load: $cpu_fin%
-        Last boot: $lb
+        CPU physical: $cpu_phy
+        vCPU: $cpu_vir
+        RAM Available: $ram_available
+        RAM Usage: $ram_percent%
+        Disk Available: $disk_available
+        Disk Usage: $disk_percent%
+        CPU load: $cpu%
+        Last boot: $last_boot
         LVM use: $lvmu
         Connections TCP: $tcpc ESTABLISHED
         User log: $ulog
-        Network: IP $ip ($mac)
-        Sudo: $cmnd cmd"
-
-
